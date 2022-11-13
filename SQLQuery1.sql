8f483e72d6b1c51f98813aaca10fe2698c8d9453
rachl <4116179@gmail.com>
2022-11-13 17:47:22 +0200

    dd tables

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,4 @@
---create database zapHear
+create database zapHear
 create table customer
 (
 email varchar(30) primary key,
@@ -12,5 +12,25 @@ id int primary key,
 depName varchar(10),
 img image  
 )
+create table location
+(
+idLocation int  not null IDENTITY(1,1) primary key,
+myColumn int,
+shelf int
+)
+create table product
+(
+barcode varchar(20) not null primary key,
+productName varchar(25),
+compqany varchar(12),
+countG int ,
+kosher varchar(10),
+price int,
+img image,
+idLocation int,
+isSale BIT,
+presentToSale int
+)
+
 
 
