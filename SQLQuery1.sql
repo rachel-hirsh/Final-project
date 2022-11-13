30d5b3f32ef2e8e0197075405ba2cbffe4555561
rachl <4116179@gmail.com>
2022-11-13 17:08:37 +0200

    add file

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+--create database zapHear
+create table customer
+(
+email varchar(30) primary key,
+pasword varchar(10) ,
+firstName varchar(15),
+lastName varchar(15),
+)
+create table department
+(
+id int primary key,
+depName varchar(10),
+img image  
+)
+
+
